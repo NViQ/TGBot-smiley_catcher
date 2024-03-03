e613b26caec961ffc6d7b9e965e16ec7e6fb9605
Павел <naveq4358@gmail.com>
2024-03-03 19:38:23 +0300

    del all classes add new func handle_text_message

--- smileys_catcher/handlers/commands.py
+++ smileys_catcher/handlers/commands.py
@@ -1,5 +1,6 @@
 from aiogram import F, types, Router
 from aiogram.filters import Command
+from aiogram.exceptions import TelegramForbiddenError
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from .utils import handle_like_reaction, handle_poo_reaction, handle_ok_hand_reaction, \
     handle_rolling_on_the_floor_laughing_reaction
@@ -7,29 +8,35 @@ from .utils import handle_like_reaction, handle_poo_reaction, handle_ok_hand_rea
 
 router = Router()
 
-
 @router.message(Command('start'))
 async def send_welcome(message: types.Message):
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="👍", callback_data="like"),
-         InlineKeyboardButton(text="👎", callback_data="dislike"),
-         InlineKeyboardButton(text="👌", callback_data="ok_hand"),
-         InlineKeyboardButton(text="🤣", callback_data="rolling_on_the_floor_laughing")]
-    ])
-    await message.answer("Hi! How do you feel about this message?", reply_markup=keyboard)
+    try:
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="👍", callback_data="like"),
+             InlineKeyboardButton(text="👎", callback_data="dislike"),
+             InlineKeyboardButton(text="👌", callback_data="ok_hand"),
+             InlineKeyboardButton(text="🤣", callback_data="rolling_on_the_floor_laughing")]
+        ])
+        await message.answer("Hi! How do you feel about this message?", reply_markup=keyboard)
+    except TelegramForbiddenError as e:
+        print(f"The chat is unavailable: {e}")
 
 
 @router.message()
 async def handle_text_message(message: types.Message):
-    if "💩" in message.text:
-        response = handle_poo_reaction()
-        await message.answer(response)
-    elif "👍" in message.text:
-        response = handle_like_reaction()
-        await message.answer(response)
-    elif "👌" in message.text:
-        response = handle_ok_hand_reaction()
-        await message.answer(response)
-    elif "🤣" in message.text:
-        response = handle_rolling_on_the_floor_laughing_reaction()
-        await message.answer(response)
+    try:
+        if message.text and "💩" in message.text:
+            response = handle_poo_reaction()
+            await message.answer(response)
+        elif message.text and "👍" in message.text:
+            response = handle_like_reaction()
+            await message.answer(response)
+        elif message.text and "👌" in message.text:
+            response = handle_ok_hand_reaction()
+            await message.answer(response)
+        elif message.text and "🤣" in message.text:
+            response = handle_rolling_on_the_floor_laughing_reaction()
+            await message.answer(response)
+
+    except TelegramForbiddenError as e:
+        print(f"The chat is unavailable: {e}")
